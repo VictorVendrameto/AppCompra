baaf5c29889796d00a38f342f2d6fac803969d03
VictorVendrameto <vendrametovictor@gmail.com>
2022-11-16 14:47:07 -0300

    Adicionar arquivos de projeto.

---
+++ AppCompra/Helper/SQLiteDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks; 
+using AppCompras.Model;
+using SQLite;
+
+namespace AppCompras.Helper
+{
+    
+    public class SQLiteDB
+    {
+        readonly SQLiteAsyncConnection _conec;
+        public SQLiteDBH(string path)
+        {
+            _conec = new SQLiteAsyncConnection(path);
+            _conec.CreateTableAsync<Produto>().Wait();
+        }
+
+
+        //insert
+        public Task<int> insert(Produto p)
+        {
+            return _conec.InsertAsync(p);
+        }
+
+
+        //Update
+        public void update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Desc=?, Quant=?, Preco=?"
+        }
+
+
+        //getById
+        public Task<Produto> getById(int id)
+        {
+            return new Produto();
+        }
+
+
+        //getAll
+        public Task<List<Produto>> getAll()
+        {
+
+        }
+
+
+        //delete
+        public void delete(int id)
+        {
+
+        }
+    }
+}
---
+++ AppCompra/Model/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using SQLite; 
+
+namespace AppCompras.Model
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        public string desc { get; set; }
+        public double preco { get; set; }
+        public double quant { get; set; }
+    }
+}
